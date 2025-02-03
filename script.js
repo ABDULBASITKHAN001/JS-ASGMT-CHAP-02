2561a8e19616e26675777e8f18afd22da9de34fb
ABDULBASITKHAN001 <[basitkhurramkhan@gmail.com]>
2025-02-03 13:18:53 +0500

    JS-ASGMT-CHAP-02

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log("HELLO-WORLD");
+ 
+// CHAPTER 02: VARIABLES FOR STRINGS 
+// QUESTION NUMBER 01:
+console.log("QUESTION NUMBER 01:");
+var username = "Abdul Basit";
+console.log(username);
+
+// QUESTION NUMBER 02:
+console.log("");
+console.log("QUESTION NUMBER 02:")
+var firstname = "ABDUL BASIT"
+var lastname = "KHAN"
+var myName = firstname + lastname;
+console.log(myName);
+
+// QUESTION NUMBER 03:
+var message = "HELLO WORLD"
+alert(message)
+
+// QUESTION NUMBER 04:
+var name = "ABDUL BASIT";
+var age = "18 years old";
+var course = "Certified Web And App Developer";
+alert(name);
+alert(age);
+alert(course);
+
+// QUESTION NUMBER 05:
+var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(pizza)
+
+// QESTION NUMBER 06:
+var email = "basitkhurramkhan@gmail.com"
+alert("My email address is " + email);
+
+// QUESTION NUMBER 07:
+var book ="A smarter way to learn JavaScript."
+alert("I'm trying to learn " + book);
+
+// QUESTION NUMBER 08: 
+var doc = "Yah! I can write HTML content through JavaScript";
+document.write(doc);
+
+// QUESTION NUMBER 09:
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+///////////////////////// CHAPTER 02 COMPLETED! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
